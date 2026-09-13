1aeec0a220cc1bb64324b29bc764f0232b112d8c
DON <167806587+donoliva2@users.noreply.github.com>
2026-09-13 17:38:22 +0530

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'DONxDEV'
-description 'DONxDEV Anti-Prop, Screenshot & Detection System'
+description 'DONxDEV ANTICHEAT - Prop Spawn Gurd, Screenshot & Detection System'
 version '1.0.0'
 
 client_scripts {
